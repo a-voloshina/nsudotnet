c43e071bfe17f680b403d45cb51ecbcd9309faa9
avoloshina <a.voloshina@g.nsu.ru>
2019-04-29 23:32:26 +0700

    Effective Worker
    -> fix seniority migration cs

--- EffectiveWorker/EffectiveWorker/Migrations/20190429162911_AddWorkerSeniority.cs
+++ EffectiveWorker/EffectiveWorker/Migrations/20190429162911_AddWorkerSeniority.cs
@@ -6,18 +6,10 @@ namespace EffectiveWorker.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "Seniority",
-                table: "Workers",
-                nullable: false,
-                defaultValue: 0);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Seniority",
-                table: "Workers");
         }
     }
 }
